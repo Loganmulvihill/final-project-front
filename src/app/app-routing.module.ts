5b14ff3d6c10e8f3338d96f45f5da79a5a00eb60
loganmulvihill <mulvihill.logan@gmail.com>
2019-12-09 18:34:08 -0800

    12-9-19

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import {MainComponent} from './main/main.component';
 import {FavoritesComponent} from './favorites/favorites.component';
 import {LoginComponent} from './login/login.component';
+import {AppComponent} from '../app/app.component';
 
 const routes: Routes = [
-
+  {path: 'app', component:AppComponent},
   {path: 'main', component: MainComponent},
   {path: 'favorites', component: FavoritesComponent},
   {path: 'login', component: LoginComponent}
