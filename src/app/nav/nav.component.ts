cd2648505268161ce758a75492bbb06ac16779a2
loganmulvihill <mulvihill.logan@gmail.com>
2019-12-16 14:59:15 -0800

    favorites start

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from '../api.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+movies;
+input;
+imageUrl= 'https://image.tmdb.org/t/p/w500'
+genres
+
+  constructor(public _api: ApiService) { }
 
   ngOnInit() {
+  }
+
+
+  onClick () {
+
+    this._api.getApi(this.input) 
+    .subscribe(result => {
+      this.movies=result;
+      console.log(this.movies)
+    })
+
+
+
   }
 
 }
